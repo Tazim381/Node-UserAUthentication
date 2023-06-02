e5d165296c3a10d700d4c1bc4fe67d6841f50014
Tazim381 <jucse28.381@gmail.com>
2023-06-02 19:26:42 +0600

    all request added

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+const users = [];
+let id = 0;
+app.get("/", (req, res) => {
+  res.status(200).json(users);
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id
+  const user= users.find((user) => user.id == id)
+  if(user) {
+    res.json(user)
+  } else {
+    res.status(404).json({message:'user not found'})
+  }
+    
+});
+
+app.post("/", (req, res) => {
+  const user = req.body;
+  user.id = id++
+  users.push(user);
+  res.status(201).json(user);
+  
+});
+
+app.put("/users/:id", (req, res) => {
+  const id = req.params.id
+  const body= req.body
+  const user = users.find((user)=> user.id== id)
+  if(user) {
+        user.fname= body.fname
+        user.lname =body.lname
+        res.json(user)
+  }else{
+    res.status(404).json({message:'user not found'})
+  }
+  }
+);
+
+app.delete("/users/:id",( req, res) =>{
+
+  const id = req.params.id;
+  const userIndex = users.findIndex((user)=> user.id== id)
+  if(userIndex) {
+    users.splice(userIndex,1);
+    res.json(users)
+  }
+
+})
+
+app.listen(port, () => {
+  console.log("app is running ");
+});
