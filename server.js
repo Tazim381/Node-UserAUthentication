4153e5cee3370c1bf3db573435e4fc945c77893e
Tazim381 <jucse28.381@gmail.com>
2023-06-15 22:19:34 +0600

    profile authentication added

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 const { Schema } = mongoose;
 //env file theke ansi ejnno ante hoise
 var bodyParser = require("body-parser");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const port = process.env.PORT
 
@@ -12,7 +14,8 @@ const port = process.env.PORT
 const blogSchema = new Schema({
   fname: String, // String is shorthand for {type: String}
   lname: String,
-  email: String
+  email: String,
+  password:String,
 });
 //create model 
 const User = mongoose.model('User', blogSchema);
@@ -42,7 +45,7 @@ app.get("/users",async(req, res) => {
 
 app.get("/users/:id", async(req, res) => {
   try{
-    const id = req.params.id
+    const id = req.params.id 
     const user= await User.findById(id)
     if(user) {
       res.json(user)
@@ -57,9 +60,18 @@ app.get("/users/:id", async(req, res) => {
 });
 
 app.post("/users", async(req, res) => {
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(req.body.password,salt)
+  const password = hash
   
   try{
-    const user = new User(req.body);
+    const userObj ={
+      fname:req.body.fname,
+      lname:req.body.lname,
+      email:req.body.email,
+      password:password
+    }
+    const user = new User(userObj);
   await user.save()
   res.status(201).json(user);
   }  catch(error) {
@@ -69,6 +81,65 @@ app.post("/users", async(req, res) => {
   
 });
 
+app.post("/users/login",async(req,res) =>{
+  try{
+     const {email,password} = req.body
+     const user = await User.findOne({email:email})
+     if(!user) {
+      res.status(401).json({message:"user not found"})  
+     }
+     else{
+      const isValidPassword = await bcrypt.compare(password,user.password)
+      if(!isValidPassword) {
+        res.status(401).json({message:"password invalid"})
+      } else {
+        //generate token
+          const token = jwt.sign({email:user.email, id:user._id},process.env.JWT_SECRET)
+          const userObj = user.toJSON()
+          userObj['token'] = token
+          res.status(201).json(userObj);   
+      }
+     }
+  }catch(error) {
+    res.status(500).json({message:"login failed"})
+    
+  }
+})
+
+//middlewire to authenticate JWT access token
+
+const authenticateToken = (req,res,next) => {
+
+  const authHeader = req.headers.authorization
+  const token = authHeader && authHeader.split(' ')[1]
+  if(!token) {
+    res.status(401).json({message:"unAuthorized user"})
+    return
+  } else {
+    jwt.verify(token,process.env.JWT_SECRET,(error,user) =>{
+      if(error) {
+        res.status(401).json({message:"unAuthorized user"})
+      } else {
+        req.user = user
+        next()
+      }
+    })
+  }
+}
+
+app.get('/profile',authenticateToken,async(req,res) =>{
+  try{
+    const id = req.user.id 
+    const user= await User.findById(id)
+    if(user) {
+      res.json(user)
+    } else {
+      res.status(404).json({message:'user not found'})
+    }
+  } catch(error){
+    res.status(500).json({message:"id dea khuje paoa jyni"})
+  }
+})
 app.put("/users/:id", async(req, res) => {
 
   try{
